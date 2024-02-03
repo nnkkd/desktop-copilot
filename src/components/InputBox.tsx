78522b505345299c40245896083316355bbba941
nnkkd <t.n.mailaddmain@gmail.com>
2024-02-03 22:47:53 +0900

    :sparkles: Refactor InputBox component layout

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,10 +1,17 @@
 const InputBox = () => {
   return (
-    <div className="m-4 flex border border-primary">
-      <textarea className="flex-1 border-r border-gray-300" />
-      <button className="flex-none border border-primary px-4 ml-2">
-        送信
-      </button>
+    <div className="flex items-center">
+      <div className="flex items-center h-12">
+        <button className="m-2 p-2 border border-primary h-full">
+          新しいスレッド
+        </button>
+      </div>
+      <div className="grow m-4 flex border border-primary items-center">
+        <textarea className="flex-1 border-r border-gray-300" />
+        <button className="h-12 flex-none border border-primary px-4 ml-2">
+          送信
+        </button>
+      </div>
     </div>
   );
 };
